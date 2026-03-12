00d6aedf3f721cd56aa5cf5b067268bf003729a5
Lucas Ramos <lucasramosdev@gmail.com>
2026-03-12 15:56:42 -0300

    Only two fruits

--- src/main/java/br/com/fruits/service/FruitService.java
+++ src/main/java/br/com/fruits/service/FruitService.java
@@ -14,12 +14,12 @@ public class FruitService {
   public List<Fruit> findAll() {
     var apple = new Fruit("Apple", "Red", MIDDLE_SIZE);
     var banana = new Fruit("Banana", "Yellow", MIDDLE_SIZE);
-    var grapes = new Fruit("Grapes", "Purple", "Small");
-    var orange = new Fruit("Orange", "Orange", MIDDLE_SIZE);
-    var mango = new Fruit("Mango", "Reddish", MIDDLE_SIZE);
-    var watermelon = new Fruit("Watermelon", "Green", "Large");
-    var pineapple = new Fruit("Pineapple", "Yellow", "Large");
-    var strawberry = new Fruit("Strawberry", "Red", "Small");
+    // var grapes = new Fruit("Grapes", "Purple", "Small");
+    // var orange = new Fruit("Orange", "Orange", MIDDLE_SIZE);
+    // var mango = new Fruit("Mango", "Reddish", MIDDLE_SIZE);
+    // var watermelon = new Fruit("Watermelon", "Green", "Large");
+    // var pineapple = new Fruit("Pineapple", "Yellow", "Large");
+    // var strawberry = new Fruit("Strawberry", "Red", "Small");
 
     return Stream.of(apple, banana, grapes, orange, mango, watermelon, pineapple, strawberry)
         .sorted(Comparator.comparing(Fruit::getName))
