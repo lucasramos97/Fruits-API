27aa732bc534b3f46365d42fc325ce5e639c4557
Lucas Ramos <lucasramosdev@gmail.com>
2026-03-12 15:02:26 -0300

    Only one fruit

--- src/main/java/br/com/fruits/service/FruitService.java
+++ src/main/java/br/com/fruits/service/FruitService.java
@@ -1,9 +1,7 @@
 package br.com.fruits.service;
 
 import br.com.fruits.model.Fruit;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,16 +11,15 @@ public class FruitService {
 
   public List<Fruit> findAll() {
     var apple = new Fruit("Apple", "Red", MIDDLE_SIZE);
-    var banana = new Fruit("Banana", "Yellow", MIDDLE_SIZE);
-    var grapes = new Fruit("Grapes", "Purple", "Small");
-    var orange = new Fruit("Orange", "Orange", MIDDLE_SIZE);
-    var mango = new Fruit("Mango", "Reddish", MIDDLE_SIZE);
-    var watermelon = new Fruit("Watermelon", "Green", "Large");
-    var pineapple = new Fruit("Pineapple", "Yellow", "Large");
-    var strawberry = new Fruit("Strawberry", "Red", "Small");
+    // var banana = new Fruit("Banana", "Yellow", MIDDLE_SIZE);
+    // var grapes = new Fruit("Grapes", "Purple", "Small");
+    // var orange = new Fruit("Orange", "Orange", MIDDLE_SIZE);
+    // var mango = new Fruit("Mango", "Reddish", MIDDLE_SIZE);
+    // var watermelon = new Fruit("Watermelon", "Green", "Large");
+    // var pineapple = new Fruit("Pineapple", "Yellow", "Large");
+    // var strawberry = new Fruit("Strawberry", "Red", "Small");
 
-    return Stream.of(apple, banana, grapes, orange, mango, watermelon, pineapple, strawberry)
-        .sorted(Comparator.comparing(Fruit::getName)).toList();
+    return List.of(apple);
   }
 
 }
